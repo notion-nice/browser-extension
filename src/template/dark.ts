c9cf8e240d7e567e2fbc7f95344a4448c4ddf8e9
SengMitnick <sengmitnick@163.com>
2024-02-21 21:57:27 +0800

    添加暗黑模式

---
+++ src/template/dark.ts
@@ -0,0 +1,22 @@
+export default `/*默认样式，最佳实践*/
+.dark #nice,
+.dark #nice p,
+.dark #nice h1,
+.dark #nice h2,
+.dark #nice h3,
+.dark #nice h4,
+.dark #nice h5,
+.dark #nice h6,
+.dark #nice ul,
+.dark #nice ol,
+.dark #nice blockquote p,
+.dark #nice strong,
+.dark #nice .table-of-contents a,
+.dark #nice em,
+.dark #nice em strong,
+.dark #nice del,
+.dark #nice .code-snippet__fix .code-snippet__line-index li,
+.dark #nice .footnote-item p {
+  color: white;
+}
+`;
